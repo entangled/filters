7fce94c75fce21f94a6febe0c676fdcc15131d30
Johan Hidding <j.hidding@esciencecenter.nl>
2019-12-02 11:14:57 +0100

    pin panflute version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
           "jupyter_client>=5.3.4,<6"
         , "pampy>=0.3.0,<0.4"
-        , "panflute>=1.11.2,<2"
+        , "panflute>=1.11.2,<1.12"
     ],
     tests_require=test_deps,
     extras_require={
